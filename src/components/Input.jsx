14a7ae5ed9b9ab635c629f1f93de40f6a63df72e
Himanshuxyzz <himanshuxyzz@gmail.com>
2023-11-23 12:59:27 +0530

    chores: input bg fix ✅

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,7 +4,7 @@ const Input = ({placeholder,type}) => {
   return (
     <input
       placeholder={placeholder}
-      className="text-white text-1xl outline-white rounded-sm w-full px-6 sm:px-3 py-4 bg-neutral-400/50 "
+      className="text-white text-1xl outline-white rounded-sm w-full px-6 sm:px-3 py-4 bg-neutral-700/50"
       type={type}
     />
   );
