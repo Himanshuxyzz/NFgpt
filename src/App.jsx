54aa259af45d0b5fb129e83e8b2eedbe92b78d6e
Himanshuxyzz <himanshuxyzz@gmail.com>
2023-12-29 00:47:35 +0530

    feat: implementing global state management redux ✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import MainLayout from "./components/main/MainLayout";
+import { Provider } from "react-redux";
+import store from "./utils/store";
 
 const App = () => {
-  return <MainLayout />;
+  return (
+    <Provider store={store}>
+      <MainLayout />
+    </Provider>
+  );
 };
 
 export default App;
---
+++ src/utils/userSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: null, //initially there are no user
+  reducers: {
+    addUser: (state, action) => {
+      return action.payload;
+    },
+    RemoveUser: (state, action) => {
+      return null;
+    },
+  },
+});
+
+
+export const {addUser,RemoveUser} = userSlice.actions
+export default userSlice.reducer
